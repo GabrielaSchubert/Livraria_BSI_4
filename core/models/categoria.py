d4746e83b29cb13bc5932d114c6bfc49a270de3f
Gabriela Schubert <gabrielaschubert172@gmail.com>
2024-10-10 11:16:47 -0300

    Criação da API para Categoria

--- core/models/categoria.py
+++ core/models/categoria.py
@@ -1,5 +1,7 @@
 from django.db import models
 
 class Categoria(models.Model):
-    descricao = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100, verbose_name= "descrição")
     
+    def __str__(self):
+        return self.descricao
