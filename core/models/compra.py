20a0a1d88514a2963a12adfc8062d1a62892a08b
Gabriela Schubert <gabrielaschubert172@gmail.com>
2024-12-12 11:57:40 -0300

    Adicionando tipo de pagamento à entidade de Compra

--- core/models/compra.py
+++ core/models/compra.py
@@ -18,9 +18,21 @@ class Compra(models.Model):
     def total(self):
         return sum(item.preco * item.quantidade for item in self.itens.all())
 
+class TipoPagamento(models.IntegerChoices):
+        CARTAO_CREDITO = 1, "Cartão de Crédito"
+        CARTAO_DEBITO = 2, "Cartão de Débito"
+        PIX = 3, "PIX"
+        BOLETO = 4, "Boleto"
+        TRANSFERENCIA_BANCARIA = 5, "Transferência Bancária"
+        DINHEIRO = 6, "Dinheiro"
+        OUTRO = 7, "Outro"
+
+tipo_pagamento = models.IntegerField(choices=TipoPagamento.choices, default=TipoPagamento.CARTAO_CREDITO)
+
 class ItensCompra(models.Model):
     compra = models.ForeignKey(Compra, on_delete=models.CASCADE, related_name="itens")
     livro = models.ForeignKey(Livro, on_delete=models.PROTECT, related_name="+")
     quantidade = models.IntegerField(default=1)
     preco = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    
     
